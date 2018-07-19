3d9998239af87e553623120d78e10c6df85fd450
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-07-19 20:20:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 function add(num1,num2){
   return (num1+num2)
 }
+
+function subtract(num1,num2){
+  return (num2-num1)
+}
+
+function multiply(num1,num2){
+  return (num1*num2)
+}
+
+function divide(num1,num2){
+  return (num1/num2)
+}
+
+function inc(num1){
+  return num1++
+}
