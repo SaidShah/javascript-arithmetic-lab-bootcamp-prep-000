0988b399c740cd20d9e9273334472de1af344374
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-07-19 20:23:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,26 @@ function divide(num1,num2){
   return (num1/num2)
 }
 
-function inc(num1){
-  return num1++
+function inc(n){
+  return n++
+}
+
+function dec(n){
+  return n--
+}
+
+function makeInt(num1){
+  return parseInt(num1,10)
+}
+
+function makeInt(num1){
+  return parseInt(num1)
+}
+
+function preserveDecimal(num1){
+  return parseFloat(num1)
+}
+
+function makeInt(num1){
+  return parseInt(num1)
 }
